428c1b0e1ebc364cb8137fc77f87e83c18b6d6df
Singh <aisingh@teksystems.com>
2020-02-12 17:36:26 +0530

    soft delete code added

---
+++ src/main/java/com/tek/trp/exception/AlreadyDeactivateAccountException.java
@@ -0,0 +1,9 @@
+package com.tek.trp.exception;
+
+public class AlreadyDeactivateAccountException extends Throwable {
+
+    public AlreadyDeactivateAccountException(String message){
+        super(message);
+
+    }
+}
