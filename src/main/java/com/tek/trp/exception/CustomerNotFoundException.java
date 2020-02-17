4ae881832ca0d1fc643a0969ebe0af1de6126b17
raadari <raadari@HSC-PG01Y4XT.allegisgroup.com>
2020-02-17 17:49:32 +0530

    removed unused files

--- src/main/java/com/tek/trp/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tek.trp.exception;
-
-public class CustomerNotFoundException extends Exception{
-
-    public CustomerNotFoundException(String message){
-        super(message);
-
-    }
-}
