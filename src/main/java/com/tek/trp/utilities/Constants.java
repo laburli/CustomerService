340cb6978f346253e65ec01b2b1c8d71fccd0be4
raadari <raadari@HSC-PG01Y4XT.allegisgroup.com>
2020-03-11 15:51:52 +0530

    added constants

---
+++ src/main/java/com/tek/trp/utilities/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * This class is all about definition of constant values
+ */
+package com.tek.trp.utilities;
+
+/**
+ * @author raadari
+ *
+ */
+public class Constants {
+	
+	public static final String PATH="api";
+	public static final String SUCCESS="Updation Successfull";
+	
+
+}
